86bea45679152c2e6ccef81546a1b098bd96eaa4
SoukainaNadir <soukainanadir33@gmail.com>
2023-06-10 21:51:40 +0100

    start activity

--- app/src/main/java/com/example/studenttrack/StartActivity.java
+++ app/src/main/java/com/example/studenttrack/StartActivity.java
@@ -2,7 +2,11 @@ package com.example.studenttrack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class StartActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        Button startButton = findViewById(R.id.start_button);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
+
+
+
+
 }
